cb71868a93130301803d91dbc54438585c916a35
Janaka dev <janakchamantha12@gmail.com>
2024-08-15 18:20:23 +0530

    other:server actions for data from server

--- serverActions/amazonscraper.ts
+++ serverActions/amazonscraper.ts
@@ -1,64 +1,91 @@
 'use server'
+import { v4 as uuid } from 'uuid'
 
 import axios from 'axios'
-const username = process.env.USER_NAME
-const password = process.env.PASSWORD
-
-const auth = Buffer.from(`${username}:${password}`).toString('base64')
-
+import { DataError, DataFile, DataFileListItem } from '@/type'
+const SCRAPER_SERVER = process.env.SCRAPER_SERVER
 export async function getDataFilesLIst() {
     try {
-        const response = await axios.get('http://161.35.168.4/data', {
-            headers: {
-                Authorization: `Basic ${auth}`,
+        const response = await axios.get(`${SCRAPER_SERVER}/data`, {
+            auth: {
+                username: process.env.USER_NAME!,
+                password: process.env.PASSWORD!,
             },
         })
-        return response
+
+        const newResponse: DataFileListItem[] = response.data.map((item) => {
+            const keys = item.name.split('_')
+
+            return {
+                filename: item.name,
+                keyword: keys[2],
+                jobid: keys[3],
+            }
+        })
+
+        return newResponse
     } catch (error) {
         console.log(error)
-        return { data: { data: 'need credentials' } }
+        return null
     }
 }
 
-export async function getDataFile(file: string) {
+export async function getJobList() {
     try {
-        const response = await axios.get(`http://161.35.168.4/data/${file}`, {
-            headers: {
-                Authorization: `Basic ${auth}`,
-            },
-        })
-        return response
-    } catch (error) {
-        console.log(error)
-        return { data: { data: 'need credentials' } }
-    }
-}
-
-export async function scrapeData(keyword: string) {
-    console.log(auth)
-    const data = new URLSearchParams()
-
-    data.append('keyword', '')
-    data.append('project', 'amazonscraper')
-    data.append('spider', 'amazonspider')
-
-    try {
-        // curl http://localhost:6800/schedule.json -d arg1=val1 -d project=myproject -d spider=somespider
-        const response = await axios.post(
-            'http://161.35.168.4/schedule.json',
-            data,
+        const response = await axios.get(
+            `${SCRAPER_SERVER}/listjobs.json?project=amazonscraper`,
             {
                 auth: {
-                    username: username!,
-                    password: password!,
-                },
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    // Authorization: `Basic ${auth}`,
+                    username: process.env.USER_NAME!,
+                    password: process.env.PASSWORD!,
                 },
             }
         )
         return response
+    } catch (error) {
+        console.log(error)
+        return { data: { data: 'need credentials' } }
+    }
+}
+export async function getDataFile(
+    filename: string
+): Promise<DataFile[] | DataError> {
+    try {
+        const response = await axios.get(`${SCRAPER_SERVER}/data/${filename}`, {
+            auth: {
+                username: process.env.USER_NAME!,
+                password: process.env.PASSWORD!,
+            },
+        })
+
+        return response.data
+    } catch (error) {
+        console.log(error)
+        return { message: 'credentials error' }
+    }
+}
+
+export async function scrapeData(keyword: string) {
+    const jobid = uuid()
+
+    const data = new URLSearchParams()
+
+    data.append('keyword', `${keyword}_${jobid}`)
+    data.append('jobid', `${jobid}`)
+    data.append('project', 'amazonscraper')
+    data.append('spider', 'amazonspider')
+
+    try {
+        await axios.post(`${SCRAPER_SERVER}/schedule.json`, data, {
+            auth: {
+                username: process.env.USER_NAME!,
+                password: process.env.PASSWORD!,
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        })
+        return null
     } catch (error) {
         console.log(error)
         return { data: { data: 'need credentials' } }
