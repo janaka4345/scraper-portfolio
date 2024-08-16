fb6662e6a5258161f87b4b3217645be039991db6
Janaka dev <janakchamantha12@gmail.com>
2024-08-16 06:22:45 +0530

    feat:page for amazon scraper

--- app/amazonscraper/page.tsx
+++ app/amazonscraper/page.tsx
@@ -1,21 +1,33 @@
-import { getDataFilesLIst, scrapeData } from "@/serverActions/amazonscraper"
-import Link from "next/link"
+import { getJobList } from "@/serverActions/amazonscraper"
+import { JobLIst } from "@/type"
+import { revalidatePath } from "next/cache"
+import ScrapeJob from "./_components/ScrapeJob"
+import MainTable from "./MainTable"
 
+export const revalidate = 10
 export default async function page() {
-    const { data } = await getDataFilesLIst()
-    const res = await scrapeData("")
+
+    const { data: jobs } = await getJobList()
+    const pendingJobs: JobLIst[] = jobs.pending
+    const runningJobs: JobLIst[] = jobs.running
+    const finishedJobs: JobLIst[] = jobs.finished
+    revalidatePath('/amazonscraper')
 
     return (
         <section>
-            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-            <div className="flex flex-col gap-2">
-                {/*@ts-ignore*/
-                    data.map(item => (
-                        <Link key={item.name} href={`/amazonscraper/${item.name}`}>{item.name}</Link>
-                    ))}
+            <ScrapeJob />
+            <h1>Pending jobs</h1>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
+                <MainTable data={pendingJobs} status='pending' />
             </div>
-            <div>
-                {JSON.stringify(res.data)}
+
+            <h1>Running jobs</h1>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
+                <MainTable data={runningJobs} status='running' />
+            </div>
+            <h1>Finished jobs</h1>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
+                <MainTable data={finishedJobs} status='finished' />
             </div>
 
         </section>
