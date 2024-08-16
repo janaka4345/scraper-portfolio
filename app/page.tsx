04e5d268023921bcb204fae5bbd397855dc7c475
Janaka dev <janakchamantha12@gmail.com>
2024-08-16 19:50:51 +0530

    other:toaster added and layout changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Footer from "@/components/custom/Footer";
 import Hero from "@/components/custom/Hero";
-import Navbar from "@/components/custom/Navbar";
 import Projects from "@/components/custom/Projects";
 import Services from "@/components/custom/Services";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Services />
       <Projects />
