7fdc551c01e19d0810a75204f47082f1d2b9b352
Janaka dev <janakchamantha12@gmail.com>
2024-08-17 22:33:19 +0530

    other:better ui

--- app/amazonscraper/page.tsx
+++ app/amazonscraper/page.tsx
@@ -1,7 +1,7 @@
 import { getJobList } from "@/serverActions/amazonscraper"
 import { JobLIst } from "@/type"
 import { revalidatePath } from "next/cache"
-import ScrapeJob from "./_components/ScrapeJob"
+import ScrapeJobForm from "./_components/ScrapeJobForm"
 import MainTable from "./MainTable"
 
 export const revalidate = 10
@@ -14,18 +14,19 @@ export default async function page() {
     revalidatePath('/amazonscraper')
 
     return (
-        <section>
-            <ScrapeJob />
-            <h1>Pending jobs</h1>
+        <section className="mx-8 flex flex-col gap-4 mt-[10svh]">
+            <h1 className=" text-3xl text-gray-700 font-bold tracking-tight w-fit mx-auto mt-4 ">Scrape the Amazon.com</h1>
+            <ScrapeJobForm />
+
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
                 <MainTable data={pendingJobs} status='pending' />
             </div>
 
-            <h1>Running jobs</h1>
+
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
                 <MainTable data={runningJobs} status='running' />
             </div>
-            <h1>Finished jobs</h1>
+
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
                 <MainTable data={finishedJobs} status='finished' />
             </div>
