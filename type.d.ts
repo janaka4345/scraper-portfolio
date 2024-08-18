c5a77ae1eee24eac60f6590bcd765ed1b2716234
Janaka dev <janakchamantha12@gmail.com>
2024-08-18 23:14:37 +0530

    types added for waalmart data fetch

--- type.d.ts
+++ type.d.ts
@@ -23,6 +23,17 @@ export type DataFile = {
     bought_in_past_month: int
 }
 
+export type walmartDataFile = {
+    name: string
+    description: string
+    image_url: string
+    list_price: float
+    sell_price: float
+    product_url: string
+    rating: float
+    reviews: int
+}
+
 export type DataError = {
     message: string
 }
