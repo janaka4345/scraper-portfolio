55e249be76b92da47265bb3d44124c5f15193fe2
Janaka dev <janakchamantha12@gmail.com>
2024-08-25 22:23:21 +0530

    other:misc

---
+++ components/custom/2dCanvas/handleInputs/mouseMoved.ts
@@ -0,0 +1,7 @@
+import { P5CanvasInstance } from '@p5-wrapper/react'
+
+function mouseMoved(p5: P5CanvasInstance) {
+    // console.log(p5.frameRate())
+    // console.log('mouse moved')
+}
+export default mouseMoved
---
+++ components/custom/2dCanvas/handleInputs/mousePressed.ts
@@ -0,0 +1,6 @@
+import { type P5CanvasInstance } from '@p5-wrapper/react'
+function mousePressed(p5: P5CanvasInstance) {
+    // console.log(p5.frameRate())
+    console.log(p5)
+}
+export default mousePressed
